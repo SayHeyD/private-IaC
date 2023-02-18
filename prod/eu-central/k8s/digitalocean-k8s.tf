28311772d4f351d2982ecfbe53255f97efb88239
David Docampo <david@docampo.ch>
2023-02-18 21:21:11 +0100

    update module source

--- prod/eu-central/k8s/digitalocean-k8s.tf
+++ prod/eu-central/k8s/digitalocean-k8s.tf
@@ -7,7 +7,7 @@ data "sops_file" "sops_age_key" {
 }
 
 module "do_cluster" {
-  source = "../../../../modules/packages/do-cluster"
+  source = "https://github.com/SayHeyD/terraform-modules/tree/main/packages/do-cluster"
 
   do_token = var.do_token
   cluster_name = "prod-eu-001"
